9842aed30684bfaaec496272df159fd58273e862
Tapio Räsänen <wandrw4@gmail.com>
2015-05-13 22:24:38 +0300

    Improved fit
    Found 3 best models.

--- neural/h2o_nnet_tuning.r
+++ neural/h2o_nnet_tuning.r
@@ -1,33 +1,40 @@
 #### TUNING AND MODEL SELECTION ####
 
+# Select response and predictors
+predictors <- 2:(ncol(train0.hex))
+response <- 1
+# Checkpoint and best model:
+# dlmodel_loaded <- h2o.loadModel(h2oServer, "<path>")
+
+mallit <- list(c(897, 565, 343), c(997,665,443),c(1011,674,449),
+	c(897, 565, 897), c(997,665,997),c(1011,674,1011),
+	c(897, 897, 897), c(997,997,997),c(1011,1011,1011),
+	c(997, 897, 665), c(1011,997,897),c(997,674,449))
+
+# list(c(897, 565, 343), c(997,665,443),c(1011,674,449)) # 10 epochs = 2h näillä noin?
+# 17,85%, 18,06%,  17,72%
+
 grid_search <- h2o.deeplearning(x=predictors, y=response,
-		data=train0.hex, 
-		hidden=c(276,184,81),
+		data=train0.hex, validation = test.hex,
+		hidden=mallit,
 		activation="Tanh",
 		classification=T,
 		  hidden_dropout_ratio=c(0,0,0),
 		  input_dropout_ratio = 0,
-		  epochs=10,
+		  epochs=1.5,
 		  l1=0,
 		  l2=0,
 		  rho=0.99,
 		  epsilon=1e-10,
-		  holdout_fraction=0.1,
 		  train_samples_per_iteration = 2000,
 		  max_w2=10,
 		  balance_classes = T,
 		  rate_decay = 0.05,
-		seed=c(1,2,3,4))
-
-# epocs = 20 ja c(276,184,81) 1h per model
-# epocs = 20 ja c(276,184,81) 15min per model (?)
-# fast_mode = false?
-
-# Also test 2-layer models?
-# nfolds = 5
+		seed=2)
+		
+# next: find a model that outperforms c(xx,xx,xx) with rate_decay = 0.05 and 0 dropout
 
 #### MODEL SELECTION ####
-
 best_model1 <- grid_search@model[[1]]
 best_model2 <- grid_search@model[[2]]
 best_model3 <- grid_search@model[[3]]
@@ -43,24 +50,46 @@ bench_model_predicted2 <- as.data.frame(h2o.predict(best_model2,test.hex))[,-1]
 bench_model_predicted3 <- as.data.frame(h2o.predict(best_model3,test.hex))[,-1]
 avg_model <- (bench_model_predicted1 + bench_model_predicted2 + bench_model_predicted3)/3
 
-LogLoss(oikeat_valid, bench_model_predicted1) # n. 0.57
-LogLoss(oikeat_valid, bench_model_predicted2)
-LogLoss(oikeat_valid, bench_model_predicted3)
-LogLoss(oikeat_valid, avg_model) # 0.51 (best avg.) -> 0.55
+round(LogLoss(oikeat_valid, bench_model_predicted1),3) # 0.55
+round(LogLoss(oikeat_valid, bench_model_predicted2),3) # 
+round(LogLoss(oikeat_valid, bench_model_predicted3),3)
+round(LogLoss(oikeat_valid, avg_model),3) # 0.51 (best avg.) -> 0.508 -> # 0.505
 
 #### Utility (Average) ####
-models_to_get <- 2
-comb_model <- as.data.frame(h2o.predict(best_model1,test.hex))[,-1]
+#### Search grid 1 ####
+models_to_get <- 5
+comb_model <- as.data.frame(h2o.predict(grid_search@model[[i]],test.hex))[,-1]
 for (i in 2:models_to_get) {
 	add_model <- as.data.frame(h2o.predict(grid_search@model[[i]],test.hex))[,-1]
 	comb_model <- comb_model + add_model
 }
 comb_model <- comb_model*(1/models_to_get)
-round(LogLoss(oikeat_valid, comb_model),3)
+round(LogLoss(oikeat_valid, comb_model),3) # 0.493 #0.488 (7)
+
+# best10: 0.479
+# best9: 0.483
+# best8: 0.488
+# best7: 0.489
+# best6: 0.493
+# best5: 0.491
+# best4: 0.496
+
+# tai 20 mallia:
 
 ##################
 #### STACKING ####
 ##################
+models_to_get <- 8
+comb_model2 <- as.data.frame(h2o.predict(grid_search@model[[i]],test.hex))[,-1]
+for (i in 2:models_to_get) {
+	add_model <- as.data.frame(h2o.predict(grid_search@model[[i]],test.hex))[,-1]
+	comb_model2 <- comb_model2 + add_model
+}
+comb_model2 <- comb_model2*(1/models_to_get)
+round(LogLoss(oikeat_valid, comb_model2),3)
+
+comb_b <- comb_model*(6/9) + comb_model2*(3/9)
+round(LogLoss(oikeat_valid, comb_b),3) # 0.477
 
 #### GENERATE TRAINING DATA (METADATA) ####
 
@@ -92,12 +121,12 @@ response_e <- 1
 # Etsitään tähänkin tarkoitukseen paras malli gridillä?
 grid_ensemble <- h2o.deeplearning(x=predictors_e, y=response_e,
 		data=train.ensemble, 
-		hidden=list(c(67,67,67),c(128,67,37),c(276,271,123),c(276,184,81)),
-		activation=c("Tanh", "RectifierWithDropout"),
+		hidden=list(c(27,27,27),c(67,27,27), c(123, 67, 46)),
+		activation="Tanh",
 		classification=T,
 		  hidden_dropout_ratio=c(0,0,0),
 		  input_dropout_ratio = 0,
-		  epochs=1.5,
+		  epochs=10,
 		  l1=0,
 		  l2=0,
 		  rho=0.99,
@@ -107,7 +136,7 @@ grid_ensemble <- h2o.deeplearning(x=predictors_e, y=response_e,
 		  max_w2=10,
 		  balance_classes = T,
 		  rate_decay = 0.05,
-		seed=2)
+		seed=1)
 
 #### TEST (METADATA) ####		
 best_ensemble <- grid_ensemble@model[[1]]
@@ -115,7 +144,7 @@ best_ensemble
 best_ensemble@model$params$hidden
 
 stack_prob <- as.data.frame(h2o.predict(best_ensemble,test.ensemble))[,-1]
-round(LogLoss(oikeat_valid, stack_prob),3) # 0.601
+round(LogLoss(oikeat_valid, stack_prob),3) # 0.601 #0.82 #0.683
 
 # sampleakin voi muuttaa:
 # validation=test.hex,
